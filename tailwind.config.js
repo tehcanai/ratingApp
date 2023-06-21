c0351fce610df70f8150cad0be567e9989820352
Arif Zakuan <arif.zakuan@gmail.com>
2023-06-22 01:24:37 +0800

    twcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,10 @@
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/Components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/utils/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/utils/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   plugins: [],
 }
